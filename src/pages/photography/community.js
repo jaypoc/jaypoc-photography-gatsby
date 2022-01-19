0d13bf35c7574fd786114e91f519724d6884c1d1
Jaypoc <admin@jaypoc.com>
2022-01-18 22:31:38 -0600

    fixed community page heading

--- src/pages/photography/community.js
+++ src/pages/photography/community.js
@@ -8,7 +8,7 @@ const CommunityPage = () => {
       <div class="row">
         <div class="col-sm-12 page">
 
-          <h2>Event Photography</h2>
+          <h2>Photography Community</h2>
           
           <p>Content goes here.</p>
           
